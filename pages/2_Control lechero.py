a2d55f005a6b91439f8e8222c4f9480c17a176ab
santiagolopezR <santiago.negr08@gmail.com>
2025-12-16 13:15:27 -0500

    Fix typo in lineplot errorbar parameter

--- pages/2_Control lechero.py
+++ pages/2_Control lechero.py
@@ -63,7 +63,7 @@ df_total = pd.concat([df_arriba, df_pioneros], ignore_index=True)
 
 promedioporfinca= df_total.groupby("Fecha")["Pdcion"].mean()
 fig, ax = plt.subplots(figsize=(15,10))
-sns.lineplot(data=df_total,x="Fecha",y="Pdcion",hue="FINCA", errobar=None, marker="o")
+sns.lineplot(data=df_total,x="Fecha",y="Pdcion",hue="FINCA", errorbar=None, marker="o")
 ax.set_title("Aforo promedio por mes", fontsize=16)
 plt.xticks(rotation=45)
 ax.grid(True)
