ad1219dc16e1c90c2d7a4b11bfd0b57f0c4501c8
santiagolopezR <santiago.negr08@gmail.com>
2025-12-16 13:13:18 -0500

    Fix typo in lineplot parameters

--- pages/2_Control lechero.py
+++ pages/2_Control lechero.py
@@ -63,7 +63,7 @@ df_total = pd.concat([df_arriba, df_pioneros], ignore_index=True)
 
 promedioporfinca= df_total.groupby("Fecha")["Pdcion"].mean()
 fig, ax = plt.subplots(figsize=(15,10))
-sns.lineplot(data=df_total,x="Fecha",y="Pdcion",hue="FINCA", errobar=None, ax=ax)
+sns.lineplot(data=df_total,x="Fecha",y="Pdcion",hue="FINCA", errobar=None)
 ax.set_title("Aforo promedio por mes", fontsize=16)
 plt.xticks(rotation=45)
 ax.grid(True)
